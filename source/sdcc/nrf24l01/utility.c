5ee98922b4610fabbbf359ded5c88dd98da2ba6f
danaolcott <danaolcott@gmail.com>
2019-01-09 00:12:24 -0800

    update utility.c .h

--- source/sdcc/nrf24l01/utility.c
+++ source/sdcc/nrf24l01/utility.c
@@ -44,6 +44,31 @@ uint8_t utility_decimal2Buffer(uint16_t value, uint8_t* output)
 }
 
 
+////////////////////////////////////////////////////////
+//Convert input buffer to equivalent array of hex values
+//len = size of input buffer, returns numbytes in output buffer
+//Assumes output buffer is large enough.  Each hex output in 
+//form of "0x## ".
+uint8_t utility_data2HexBuffer(uint8_t* input, uint8_t len, uint8_t* output)
+{
+    uint8_t i = 0x00;
+    int n = 0x00;
+    uint8_t hexValue[6] = {0x00};
+    uint8_t offset = 0x00;          //output buffer offset
+
+    for (i = 0 ; i < len ; i++)
+    {
+        n = sprintf(output + offset, "0x%02x ", input[i]);
+        offset += n;
+    }
+
+    output[offset] = 0x00;      //null terminate
+
+    return offset;
+}
+
+
+
 
 
 
